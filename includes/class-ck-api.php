3f9989f0a2b99bd6d8121a3f41611f42e8ea4d4b
WPGuru4u <wpguru4u@gmail.com>
2017-02-11 13:57:01 -0500

    add wrapper for ck api

--- includes/class-ck-api.php
+++ includes/class-ck-api.php
@@ -43,26 +43,41 @@ class M_CK_API {
 
 	}
 
-	public static function add_tag( $tag_id, $options = array() ) {
-		if ( ! array_key_exists( 'email', $options ) ) {
-			self::log( 'no email' );
+	public static function add_tag( $tag_id, $email ) {
+		if ( ! $email ) {
+			self::log( 'no email for add_tag', '', '' );
 			return;
-		} 
+		}
+		$log  = "add_tag fired".PHP_EOL.
+				"Date: ".date("F j, Y, g:i a").PHP_EOL.
+				"tag_id: ".$tag_id.PHP_EOL.
+				"email: ".$email.PHP_EOL.
+				"-------------------------".PHP_EOL;
+		file_put_contents( plugin_dir_path(__FILE__) . "log_ck.txt", $log, FILE_APPEND);
+		$args = array();
+		$args['email'] = $email;
 		$request = 'v3/tags/' . $tag_id . '/subscribe';
 		return self::make_request( $request, 'POST', $args );
 	}
 
-	public static function remove_tag( $tag_id, $options = array() ) {
-		if ( ! array_key_exists( 'email', $options ) ) {
-			self::log( 'no email' );
+	public static function remove_tag( $tag_id, $email ) {
+		if ( ! $email ) {
+			self::log( 'no email for remove_tag', '', '' );
 			return;
 		} 
-
-		$subscriber_id = self::get_subscriber_id( $options['email'] );
+		$log  = "remove_tag fired".PHP_EOL.
+				"Date: ".date("F j, Y, g:i a").PHP_EOL.
+				"tag_id: ".$tag_id.PHP_EOL.
+				"email: ".$email.PHP_EOL.
+				"-------------------------".PHP_EOL;
+		file_put_contents( plugin_dir_path(__FILE__) . "log_ck.txt", $log, FILE_APPEND);
+		$subscriber_id = self::get_subscriber_id( $email );
 
 		if ( ! $subscriber_id )
 			return;
-
+		$args = array(
+			'email' => $email,
+		);
 		$request = 'v3/subscribers/' . $subscriber_id . '/tags/' . $tag_id;
 		return self::make_request( $request, 'DELETE', $args );
 	}
@@ -71,12 +86,20 @@ class M_CK_API {
 		if ( ! $email )
 			return;
 		$args = array(
-			'email' => $email,
+			'email_address' => $email,
 		);
 
 		$request = 'v3/subscribers';
 		$response = self::make_request( $request, 'GET', $args );
-		$subscriber_id = $response->subscribers->id;
+
+		$subscriber_id = $response->subscribers['0']->id;
+
+		$log  = "get_subscriber_id response".PHP_EOL.
+				"Date: ".date("F j, Y, g:i a").PHP_EOL.
+				"email: ".$email.PHP_EOL.
+				"subscriber_id: ".$subscriber_id.PHP_EOL.
+				"-------------------------".PHP_EOL;
+		file_put_contents( plugin_dir_path(__FILE__) . "log_ck.txt", $log, FILE_APPEND);
 		return $subscriber_id;
 	}
 
@@ -90,15 +113,26 @@ class M_CK_API {
 	 */
 	private static function make_request( $request, $method = 'GET', $args = array() ) {
 
+
 		$general_options = get_option('_wp_convertkit_settings');
 		$api_key = $general_options && array_key_exists('api_key', $general_options) ? $general_options['api_key'] : null;
 		$api_secret = $general_options && array_key_exists('api_secret', $general_options) ? $general_options['api_secret'] : null;
-		$debug = $general_options && array_key_exists('debug', $general_options) ? $general_options['debug'] : null;
 
 		$args['api_key'] = $api_key;
+		$args['api_secret'] = $api_secret;
 
 		$url = 'https://api.convertkit.com/' . $request . '?' . http_build_query( $args );
-		self::log( "API Request: " . $url );
+	//	$url = 'https://api.convertkit.com/' . $request;
+	//	$url = add_query_arg($args, $url);
+	//	self::log( "API Request: ", $url, $args);
+
+		$log  = "make_request fired".PHP_EOL.
+				"Date: ".date("F j, Y, g:i a").PHP_EOL.
+				"request: ".$request.PHP_EOL.
+				"method: ".$method.PHP_EOL.
+			//	"args: ".print_r($args).PHP_EOL.
+				"-------------------------".PHP_EOL;
+		file_put_contents( plugin_dir_path(__FILE__) . "log_ck1.txt", $log, FILE_APPEND);
 
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $url);
@@ -112,25 +146,25 @@ class M_CK_API {
 		$results = curl_exec($ch);
 		curl_close($ch);
 
-		self::log( "API Response: " . print_r( json_decode( $results ), true) );
-
-		return json_decode( $results );
+	//	self::log( "API Response: ", $url, json_decode( $results ) );
+		$log  = "curl closed".PHP_EOL.
+				"Date: ".date("F j, Y, g:i a").PHP_EOL.
+				"results: ".$results.PHP_EOL.
+				"-------------------------".PHP_EOL;
+		file_put_contents( plugin_dir_path(__FILE__) . "log_ck-results.txt", $log, FILE_APPEND);
+		return json_decode($results);
 	}
 
 	/**
 	 * @param $message
 	 */
-	public static function log( $message ) {
+	public static function log( $title, $var1, $var2 ) {
+		$log  = $title.PHP_EOL.
+				"Date: ".date("F j, Y, g:i a").PHP_EOL.
+				"var1: ".$var1.PHP_EOL.
+				"var2: ".print_r($var2).PHP_EOL.
+				"-------------------------".PHP_EOL;
+		file_put_contents( plugin_dir_path(__FILE__) . "log_ck1.txt", $log, FILE_APPEND);
 
-		if ( 'on' == self::$debug ) {
-			$dir = dirname( __FILE__ );
-
-			$handle = fopen( trailingslashit( $dir ) . 'log.txt', 'a' );
-			if ( $handle ) {
-				$time   = date_i18n( 'm-d-Y @ H:i:s -' );
-				fwrite( $handle, $time . " " . $message . "\n" );
-				fclose( $handle );
-			}
-		}
 	}
 }
